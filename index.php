960c6e5db39044591a1e328b17e1c001c272b316
mexx91 <maximiliano_gross@yahoo.de>
2013-06-04 22:58:25 -0700

    redirect to https (for screensharing tests)

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+if($_SERVER['HTTPS']!="on")
+  {
+     $redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+     header("Location:$redirect");
+  }
+?>
+
+
 <html>
     <head>
 
